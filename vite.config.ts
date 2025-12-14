b4a6c36198d2d8825a7e4932aeca299d06dc6672
Jean-Pierre-P <je.portino@duocuc.cl>
2025-12-13 21:43:16 -0300

    otro cambio

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+        changeOrigin: true
       }
     }
   },
   test: {
     globals: true,
     environment: 'jsdom',
-    // Esta línea es crucial, le dice a Vitest dónde está tu setup
-    setupFiles: './test/setup.ts' 
+    setupFiles: './test/setup.ts'
   }
 })
